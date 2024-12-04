d4300431d19e8ab5d43e6751a9f8cfaf16445405
sv742744 <shashi09op@gmail.com>
2024-12-04 15:17:21 +0530

    Add files via upload

---
+++ ifelse.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class ifelse{
+    public static void main (String args[]){
+        Scanner s= new Scanner(System.in);
+        int a=s.nextInt();
+        if((a%2)==0)
+            System.out.println("even");
+        else
+            System.out.println(" odd");
+    }
+}
