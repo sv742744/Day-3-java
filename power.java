f1c38cc8dd64dd9e122480cd10cba02d19ee33c3
sv742744 <shashi09op@gmail.com>
2024-12-04 11:06:10 +0530

    Add files via upload

---
+++ power.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class power{
+    public static void main (String args[]){
+        Scanner s= new Scanner(System.in);
+        int a=s.nextInt();
+        System.out.println("power of "+a+"   :"+(1<<a));
+    }
+}
